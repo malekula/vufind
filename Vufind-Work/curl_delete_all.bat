8a6428da0b46f92b5aa35beaa25896e3201daa23
corpse56 <debarkader@gmail.com>
2017-09-06 16:56:26 +0300

    изменены url для обновления индекса в связи с переездом на новый сервер.

--- Vufind-Work/curl_delete_all.bat
+++ Vufind-Work/curl_delete_all.bat
@@ -3,3 +3,5 @@ curl http://192.168.56.31:8080/solr/biblio/update --data "<delete><query>*:*</qu
 
 
 curl http://192.168.56.31:8080/solr/biblio/update --data "<commit/>" -H "Content-type:text/xml; charset=utf-8"
+
+
