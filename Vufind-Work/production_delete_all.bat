51a385db064a5150325d2ad8654e6ee60f23892e
root <root@rd-vm031.libfl.ru>
2017-11-01 15:35:22 +0300

    Исправлен файл facets.ini для правильного отображения ТПР Добавлен файл со всеми командами чистки и добавления индексов по всем источникам

--- Vufind-Work/production_delete_all.bat
+++
@@ -1,5 +0,0 @@
-curl http://catalog.libfl.ru:8080/solr/biblio/update --data "<delete><query>*:*</query></delete>" -H "Content-type:text/xml; charset=utf-8"
-
-
-
-curl http://catalog.libfl.ru:8080/solr/biblio/update --data "<commit/>" -H "Content-type:text/xml; charset=utf-8"
