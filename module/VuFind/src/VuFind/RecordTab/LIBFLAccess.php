d55490338db0e460e11248302edae13a36eecbaa
Maksim Kuleba <maksim.kuleba@gmail.com>
2018-04-26 11:04:23 +0300

    upd
    Оптимизирован код вкладки ДОСТУП

--- module/VuFind/src/VuFind/RecordTab/LIBFLAccess.php
+++ module/VuFind/src/VuFind/RecordTab/LIBFLAccess.php
@@ -19,6 +19,10 @@ use Zend\View\Renderer\PhpRenderer;
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  */
 class LIBFLAccess extends AbstractBase {
+
+    protected $access;
+    protected $accessExemplars;
+
     /**
      *
      * @return string
@@ -41,22 +45,22 @@ class LIBFLAccess extends AbstractBase {
         foreach ($accessMethods as $accessMethodCode) {
             switch ($accessMethodCode) {
                 case '4000':
-                    $access = '4_libraryAccess';
+                    $this->access = '4_libraryAccess';
                     break;
                 case '4001':
-                    $access = '3_homeAccess';
+                    $this->access = '3_homeAccess';
                     break;
                 case '4002':
-                    $access = '1_remoteAccess';
+                    $this->access = '1_remoteAccess';
                     break;
                 case '4003':
-                    $access = '2_printAccess';
+                    $this->access = '2_printAccess';
                     break;
                 case '4005':
-                    $access = '5_clarifyAccess';
+                    $this->access = '5_clarifyAccess';
                     break;
                 default:
-                    $access = '99_unknownAccess';
+                    $this->access = '99_unknownAccess';
                     break;
             }
 
@@ -105,22 +109,42 @@ class LIBFLAccess extends AbstractBase {
                         $access_group = '14_access_group_'.$exemplar->exemplar_access_group;
                         break;
                 }
-                $accessExemplars = array();
-                if (in_array($exemplar->exemplar_access, array_keys($accessExemplars[$access][$access_group]))) {
-                    $accessExemplars[$access][$access_group][$exemplar->exemplar_access] = array(
-                        'exemplar_id' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_id'], array($exemplar->exemplar_id)),
-                        'exemplar_ids' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_ids'], array($exemplar->exemplar_id)),
+
+                if (!empty($this->accessExemplars) && in_array($exemplar->exemplar_access, array_keys($this->accessExemplars[$this->access][$access_group]))) {
+                    /*$this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access] = array(
+                        'exemplar_id' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_id'], array($exemplar->exemplar_id)),
+                        'exemplar_ids' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_ids'], array($exemplar->exemplar_id)),
                         'exemplar_access_code' => $exemplar->exemplar_access,
                         'exemplar_access_group' => $exemplar->exemplar_access_group,
-                        'exemplar_location' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_location'], array($exemplar->exemplar_location)),
-                        'exemplar_rack_location' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_acces]['exemplar_rack_location'], array($exemplar->exemplar_rack_location)),
-                        'exemplar_placing_cipher' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher)),
-                        'exemplar_inventory_number' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number)),
-                        'exemplar_barcode' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access]['exemplar_barcode'], array($exemplar->exemplar_barcode)),
-                        //'exemplar_inv_note' => array_merge($accessExemplars[$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inv_note'], array($exemplar->exemplar_inv_note)),
+                        'exemplar_location' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_location'], array($exemplar->exemplar_location)),
+                        'exemplar_rack_location' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_acces]['exemplar_rack_location'], array($exemplar->exemplar_rack_location)),
+                        'exemplar_placing_cipher' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher)),
+                        'exemplar_inventory_number' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number)),
+                        'exemplar_barcode' => array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_barcode'], array($exemplar->exemplar_barcode)),
+                        //'exemplar_inv_note' => array_merge($this->accessExemplars[$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inv_note'], array($exemplar->exemplar_inv_note)),
                         'exemplar_hyperlink' => $exemplar->exemplar_hyperlink,
                         'exemplar_hyperlink_newviewer' => $exemplar->exemplar_hyperlink_newviewer,
-                    );
+                    );*/
+                    if (isset($exemplar->exemplar_id))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_id'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_id'], array($exemplar->exemplar_id));
+                    if (isset($exemplar->exemplar_access))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_access_code'] = $exemplar->exemplar_access;
+                    if (isset($exemplar->exemplar_access_group))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_access_group'] = $exemplar->exemplar_access_group;
+                    if (isset($exemplar->exemplar_location))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_location'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_location'], array($exemplar->exemplar_location));
+                    if (isset($exemplar->exemplar_rack_location))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_rack_location'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_rack_location'], array($exemplar->exemplar_rack_location));
+                    if (isset($exemplar->exemplar_placing_cipher))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_placing_cipher'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher));
+                    if (isset($exemplar->exemplar_inventory_number))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_inventory_number'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number));
+                    if (isset($exemplar->exemplar_barcode))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_barcode'] = array_merge($this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_barcode'], array($exemplar->exemplar_barcode));
+                    if (isset($exemplar->exemplar_hyperlink))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_hyperlink'] = $exemplar->exemplar_hyperlink;
+                    if (isset($exemplar->exemplar_hyperlink_newviewer))
+                        $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_hyperlink_newviewer'] = $exemplar->exemplar_hyperlink_newviewer;
                 } else {
                     if (($accessMethodCode == 4000 && in_array($exemplar->exemplar_access_group, array('6','8','9','10')) && in_array($exemplar->exemplar_access, array('1003','1005','1007','1011','1012','1014')))
                         OR ($accessMethodCode == 4001 && in_array($exemplar->exemplar_access_group, array('6','7')) && in_array($exemplar->exemplar_access, array('1000','1006','1017')))
@@ -128,7 +152,7 @@ class LIBFLAccess extends AbstractBase {
                         OR ($accessMethodCode == 4003 && in_array($exemplar->exemplar_access_group, array('5')) && in_array($exemplar->exemplar_access, array('1009')))
                         OR ($accessMethodCode == 4005 && in_array($exemplar->exemplar_access_group, array('11','12','13','99')) && in_array($exemplar->exemplar_access, array('1010','1013','1016','1999'))))
                     {
-                        $accessExemplars[$access][$access_group][$exemplar->exemplar_access] = array(
+                        /*$this->accessExemplars[$access][$access_group][$exemplar->exemplar_access] = array(
                             'exemplar_id' => [$num=>$exemplar->exemplar_id],
                             'exemplar_ids' => [$num=>$exemplar->exemplar_id],
                             'exemplar_access_code' => $exemplar->exemplar_access,
@@ -141,22 +165,42 @@ class LIBFLAccess extends AbstractBase {
                             //'exemplar_inv_note' => $exemplar->exemplar_inv_note,
                             'exemplar_hyperlink' => $exemplar->exemplar_hyperlink,
                             'exemplar_hyperlink_newviewer' => $exemplar->exemplar_hyperlink_newviewer,
-                        );
+                        );*/
+                        if (isset($exemplar->exemplar_id))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_id'] = [$num=>$exemplar->exemplar_id];
+                        if (isset($exemplar->exemplar_access))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_access_code'] = $exemplar->exemplar_access;
+                        if (isset($exemplar->exemplar_access_group))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_access_group'] = $exemplar->exemplar_access_group;
+                        if (isset($exemplar->exemplar_location))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_location'] = [$exemplar->exemplar_location];
+                        if (isset($exemplar->exemplar_rack_location))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_rack_location'] = [$exemplar->exemplar_rack_location];
+                        if (isset($exemplar->exemplar_placing_cipher))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_placing_cipher'] = [$exemplar->exemplar_placing_cipher];
+                        if (isset($exemplar->exemplar_inventory_number))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_inventory_number'] = [$exemplar->exemplar_inventory_number];
+                        if (isset($exemplar->exemplar_barcode))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_barcode'] = [$exemplar->exemplar_barcode];
+                        if (isset($exemplar->exemplar_hyperlink))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_hyperlink'] = $exemplar->exemplar_hyperlink;
+                        if (isset($exemplar->exemplar_hyperlink_newviewer))
+                            $this->accessExemplars[$this->access][$access_group][$exemplar->exemplar_access]['exemplar_hyperlink_newviewer'] = $exemplar->exemplar_hyperlink_newviewer;
                     }
                 }
 
-                /*if (in_array($exemplar->exemplar_access.'.'.$exemplar->exemplar_location, array_keys($accessExemplars[$access][$access_group]))) {
-                    $accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
-                        'exemplar_id' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_id'], array($exemplar->exemplar_id)),
-                        'exemplar_ids' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_ids'], array($exemplar->exemplar_id)),
+                /*if (in_array($exemplar->exemplar_access.'.'.$exemplar->exemplar_location, array_keys($this->accessExemplars[$access][$access_group]))) {
+                    $this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
+                        'exemplar_id' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_id'], array($exemplar->exemplar_id)),
+                        'exemplar_ids' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_ids'], array($exemplar->exemplar_id)),
                         'exemplar_access_code' => $exemplar->exemplar_access,
                         'exemplar_access_group' => $exemplar->exemplar_access_group,
                         'exemplar_location' => $exemplar->exemplar_location,
-                        'exemplar_rack_location' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_rack_location'], array($exemplar->exemplar_rack_location)),
-                        'exemplar_placing_cipher' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher)),
-                        'exemplar_inventory_number' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number)),
-                        'exemplar_barcode' => array_merge($accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_barcode'], array($exemplar->exemplar_barcode)),
-                        //'exemplar_inv_note' => array_merge($accessExemplars[$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inv_note'], array($exemplar->exemplar_inv_note)),
+                        'exemplar_rack_location' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_rack_location'], array($exemplar->exemplar_rack_location)),
+                        'exemplar_placing_cipher' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher)),
+                        'exemplar_inventory_number' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number)),
+                        'exemplar_barcode' => array_merge($this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_barcode'], array($exemplar->exemplar_barcode)),
+                        //'exemplar_inv_note' => array_merge($this->accessExemplars[$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inv_note'], array($exemplar->exemplar_inv_note)),
                         'exemplar_hyperlink' => $exemplar->exemplar_hyperlink,
                         'exemplar_hyperlink_newviewer' => $exemplar->exemplar_hyperlink_newviewer,
                     );
@@ -167,7 +211,7 @@ class LIBFLAccess extends AbstractBase {
                         OR ($accessMethodCode == 4003 && in_array($exemplar->exemplar_access_group, array('5')) && in_array($exemplar->exemplar_access, array('1009')))
                         OR ($accessMethodCode == 4005 && in_array($exemplar->exemplar_access_group, array('11','12','13','99')) && in_array($exemplar->exemplar_access, array('1010','1013','1016','1999'))))
                     {
-                        $accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
+                        $this->accessExemplars[$access][$access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
                             'exemplar_id' => [$num=>$exemplar->exemplar_id],
                             'exemplar_ids' => [$num=>$exemplar->exemplar_id],
                             'exemplar_access_code' => $exemplar->exemplar_access,
@@ -186,8 +230,8 @@ class LIBFLAccess extends AbstractBase {
             }
         }
         //print_r($aMethods);
-        ksort($accessExemplars);
-        foreach ($accessExemplars as $key => $value) {
+        ksort($this->accessExemplars);
+        foreach ($this->accessExemplars as $key => $value) {
             $access_method = preg_replace('/^\d{1,2}_/', 'access_', $key);
             $sortAccessMethod[$access_method] = $value; // Создаем новый массив, удалив информацию для сортировки ключей 'MethodOfAccess'
             ksort($sortAccessMethod[$access_method], SORT_NUMERIC); // Сортируем ключи 'GroupAccess'
