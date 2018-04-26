f90c5c2e64658ca3290c9dcf0c6e9183089c58ee
Maksim Kuleba <maksim.kuleba@gmail.com>
2018-04-26 11:23:40 +0300

    upd
    Удален закомментированный старый код.

--- module/VuFind/src/VuFind/RecordTab/LIBFLAccess.php
+++ module/VuFind/src/VuFind/RecordTab/LIBFLAccess.php
@@ -38,7 +38,7 @@ class LIBFLAccess extends AbstractBase {
         foreach ($keys as $i => $k) {
             $result[$k][] = $values[$i];
         }
-        return    $result;
+        return $result;
     }
 
     public function sortExemplars($accessMethods = array(), $exemplars = array()) {
@@ -161,45 +161,6 @@ class LIBFLAccess extends AbstractBase {
                             $this->accessExemplars[$this->access][$this->access_group][$exemplar->exemplar_access]['exemplar_hyperlink_newviewer'] = $exemplar->exemplar_hyperlink_newviewer;
                     }
                 }
-
-                /*if (in_array($exemplar->exemplar_access.'.'.$exemplar->exemplar_location, array_keys($this->accessExemplars[$access][$this->access_group]))) {
-                    $this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
-                        'exemplar_id' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_id'], array($exemplar->exemplar_id)),
-                        'exemplar_ids' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_ids'], array($exemplar->exemplar_id)),
-                        'exemplar_access_code' => $exemplar->exemplar_access,
-                        'exemplar_access_group' => $exemplar->exemplar_access_group,
-                        'exemplar_location' => $exemplar->exemplar_location,
-                        'exemplar_rack_location' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_rack_location'], array($exemplar->exemplar_rack_location)),
-                        'exemplar_placing_cipher' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_placing_cipher'], array($exemplar->exemplar_placing_cipher)),
-                        'exemplar_inventory_number' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inventory_number'], array($exemplar->exemplar_inventory_number)),
-                        'exemplar_barcode' => array_merge($this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_barcode'], array($exemplar->exemplar_barcode)),
-                        //'exemplar_inv_note' => array_merge($this->accessExemplars[$exemplar->exemplar_access.'.'.$exemplar->exemplar_location]['exemplar_inv_note'], array($exemplar->exemplar_inv_note)),
-                        'exemplar_hyperlink' => $exemplar->exemplar_hyperlink,
-                        'exemplar_hyperlink_newviewer' => $exemplar->exemplar_hyperlink_newviewer,
-                    );
-                } else {
-                    if (($accessMethodCode == 4000 && in_array($exemplar->exemplar_access_group, array('6','8','9','10')) && in_array($exemplar->exemplar_access, array('1003','1005','1007','1011','1012','1014')))
-                        OR ($accessMethodCode == 4001 && in_array($exemplar->exemplar_access_group, array('6','7')) && in_array($exemplar->exemplar_access, array('1000','1006','1017')))
-                        OR ($accessMethodCode == 4002 && in_array($exemplar->exemplar_access_group, array('1','2','3','4')) && in_array($exemplar->exemplar_access, array('1001','1002','1004','1008')))
-                        OR ($accessMethodCode == 4003 && in_array($exemplar->exemplar_access_group, array('5')) && in_array($exemplar->exemplar_access, array('1009')))
-                        OR ($accessMethodCode == 4005 && in_array($exemplar->exemplar_access_group, array('11','12','13','99')) && in_array($exemplar->exemplar_access, array('1010','1013','1016','1999'))))
-                    {
-                        $this->accessExemplars[$access][$this->access_group][$exemplar->exemplar_access.'.'.$exemplar->exemplar_location] = array(
-                            'exemplar_id' => [$num=>$exemplar->exemplar_id],
-                            'exemplar_ids' => [$num=>$exemplar->exemplar_id],
-                            'exemplar_access_code' => $exemplar->exemplar_access,
-                            'exemplar_access_group' => $exemplar->exemplar_access_group,
-                            'exemplar_location' => $exemplar->exemplar_location,
-                            'exemplar_rack_location' => [$exemplar->exemplar_rack_location],
-                            'exemplar_placing_cipher' => [$exemplar->exemplar_placing_cipher],
-                            'exemplar_inventory_number' => [$exemplar->exemplar_inventory_number],
-                            'exemplar_barcode' => [$exemplar->exemplar_barcode],
-                            //'exemplar_inv_note' => $exemplar->exemplar_inv_note,
-                            'exemplar_hyperlink' => $exemplar->exemplar_hyperlink,
-                            'exemplar_hyperlink_newviewer' => $exemplar->exemplar_hyperlink_newviewer,
-                        );
-                    }
-                }*/
             }
         }
         //print_r($aMethods);
